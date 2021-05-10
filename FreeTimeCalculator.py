b85971ea56d001d9a9c04b77d18b8ce1caa9261e
elizabethvickery <78845148+elizabethvickery@users.noreply.github.com>
2021-05-10 11:25:41 +0400

    Update FreeTimeCalculator.py

--- FreeTimeCalculator.py
+++ FreeTimeCalculator.py
@@ -1,4 +1,5 @@
 import json 
+from datetime import datetime
 
 def load_data():
     with open('data.json') as UserData:
@@ -6,42 +7,63 @@ def load_data():
         return data
 def UserData():
     User_data = {
-            "Name": [],
-            "LastName": 
+            "Name": "",
+            "LastName": "",
+            "Age":0,
+            "Occupation":"",
+            "Last_login_date":""
             }
 
-    User_data["Name"]= input("Please, type your name."))
-    User_data["LastName"]=int(input("Please, type your last name."))
-    User_data["Age"]=int(input("Please, insert your name."))
-    User_data["Occupation"]=int(input("What is your occupation?"))
-    User_data["FreeTime"]=int(input("What you think,do you have a lack of free time?"))
+    User_data["Name"]= input("Please, type your name ")
+    User_data["LastName"]=input("Please, type your last name ")
+    User_data["Age"]=int(input("Please, insert your age "))
+    User_data["Occupation"]=input("What is your occupation? ")
+    now = datetime.now()
+    User_data["Last_login_date"] = now.strftime("%d / %m / %y")
     return User_data
  
     
-def Unavailable_Time:
-    Unavailable_Time = {
+def Free_time_calculator():
+    Unavailable_Time = []
+    Unavailable_Time.append( float(input("How many hours a day do you sleep? ")))
+    Unavailable_Time.append( float(input("How many hours a day do you usually spend on your hobbies? ")))
+    Unavailable_Time.append( float(input("How many hours a day do you usually spend on your work? ")))
+    Unavailable_Time.append( float(input("How many hours a day do you usually spend on your homework? ")))
+    Unavailable_Time.append(float(input("How many hours a day you usually spend on your meals? ")))
+    sum = 0
+    i = 0
+    while(i < len(Unavailable_Time)):
+        sum = sum + Unavailable_Time[i]
+        i= i+1
 
+    while(True):
+        answer = input ("Are there any other activities you spend time on dayly? (y,n) ")
+        if (answer == "y"):
+            extra_time = float(input("How many hours a day do you spend on that activity? "))
+            sum = sum + extra_time
+        elif (answer == "n"):
+            break
+        else:
+            print("Please enter a valid answer (y,n) ")
 
+    free_time = 24 - sum 
 
-            }
-    Unavailable_Time["Amount_of_Sleep"]=float(input("How many hours a day do you sleep?"))
-    Unavailable_Time["Time_Spent_on_Hobbies"]=float(input("How many hours a day do you usually spend on your hobbies?" /)
-    Unavailable_Time["Time_Spent_on_Work"]=float(input("How many hours a day do you usually spend on your work?" /)hobbies))
-    Unavailable_Time["Time_Spent_on_Homework"]=float(input("How many hours a day do you usually spend on your homework?" /)work))
-    Unavailable_Time["Time_Spent_on_Meals"]=float(input("How many hours a day you usually spend on your meals?" /)homework))
-    Unavailable_Time["Suggestions"]=int(input("Your suggestions on what you spend your time."))
-    Unavailable_Time["Time_Spent_on_Suggestions"]=float(input("How many overall hours a day do you spend on your suggested activities?" /)meals))
-    return Unavailable_Time
+    return free_time
 
-def save_to_json():
-    x = open("data.json")
-    to_json = json.dumps(x)
+def save_to_json(Users):
+    x = open("data.json","w")
+    x.write (json.dumps(Users, indent = 2))
+    x.close()
 
-def Free_Time_Calc(Unavailable_Time):
-    sleep = Unavailable_Time["Amount_of_Sleep"] #24 - Unavailable_Time["Amount_of_Sleep"]
-    )hobby = remainder_sleep - Unavailable_Time["Time_Spent_on_Hobbies"]
-    )work = remainder_hobbies - Unavailable_Time["Time_Spent_on_Work"]
-    )homework = remainder_work - Unavailable_Time["Time_Spent_on_Homework"]
-    )meals = remainder_homework - Unavailable_Time["Time_Spent_on_Meals"]
-    your_free_time = )meals - Unavailable_Time["Time_Spent_on_Suggestions"]
+def main():
+    Users = []
+    Users = load_data()
+    J = UserData()
+    Users.append(J)
+    save_to_json(Users)
+
+    F = str(Free_time_calculator())
+    print("You have " + F + " hours of free time")
+
+main()
 
